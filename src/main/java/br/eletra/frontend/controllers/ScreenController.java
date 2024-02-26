6f40624615362302bec671841c9033e6c4caf99b
pdrflp0 <pedrofelipewk@gmail.com>
2024-02-26 12:25:24 -0300

    ### Changed    - Comments in Portuguese to English.
    ### Removed
       - Unnecessary comments.
       - Part of useless codes

--- src/main/java/br/eletra/frontend/controllers/ScreenController.java
+++ src/main/java/br/eletra/frontend/controllers/ScreenController.java
@@ -20,22 +20,16 @@ public class ScreenController {
     @FXML
     private Accordion accordion;
 
-    private final Map<String, Map<String, List<String>>> modelsPerLine = new HashMap<>(); // Mapa para armazenar os modelos por linha e subcategoria
     private final LinesInitializer linesInitializer = new LinesInitializer();
     private final ModelsInitializer modelsInitializer = new ModelsInitializer();
 
-    @FXML
     public void initialize() {
         titledPaneModels.setDisable(true);
 
         comboBoxLines.setItems(linesInitializer.lines);
         comboBoxLines.setOnAction(event -> handleLineSelection());
-
-        // Inicializar os modelos
-        start();
     }
 
-    @FXML
     private void handleLineSelection() {
         String selectedLine = comboBoxLines.getValue();
         if (selectedLine != null) {
@@ -45,32 +39,24 @@ public class ScreenController {
     }
 
     private void displayModels(String selectedLine) {
-        if (selectedLine != null) {
-            // Obtém os modelos por linha e subcategoria usando o ModelsInitializer
-            Map<String, List<String>> subcategories = modelsInitializer.initializeModels().get(selectedLine);
-            if (subcategories != null) {
-                TreeItem<String> rootItem = new TreeItem<>(selectedLine);
-                for (String subcategory : subcategories.keySet()) {
-                    TreeItem<String> subcategoryItem = new TreeItem<>(subcategory);
-                    List<String> models = subcategories.get(subcategory);
-                    for (String model : models) {
-                        subcategoryItem.getChildren().add(new TreeItem<>(model));
-                    }
-                    rootItem.getChildren().add(subcategoryItem);
+        Map<String, List<String>> subcategories = modelsInitializer.initializeModels().get(selectedLine);
+        if (subcategories != null) {
+            TreeItem<String> rootItem = new TreeItem<>(selectedLine);
+            for (String subcategory : subcategories.keySet()) {
+                TreeItem<String> subcategoryItem = new TreeItem<>(subcategory);
+                List<String> models = subcategories.get(subcategory);
+                for (String model : models) {
+                    subcategoryItem.getChildren().add(new TreeItem<>(model));
                 }
-                treeView.setRoot(rootItem);
-                expandModels();
+                rootItem.getChildren().add(subcategoryItem);
             }
+            treeView.setRoot(rootItem);
+            expandModels();
         }
-    }
-
-    public void start() {
-        // Inicializa os modelos usando o ModelsInitializer
-        modelsInitializer.initializeModels();
     }
 
     private void expandModels() {
         titledPaneModels.setExpanded(true);
-        accordion.setExpandedPane(accordion.getPanes().get(1)); // Índice 1 corresponde ao TitledPane de Modelos
+        accordion.setExpandedPane(accordion.getPanes().get(1)); // Index 1 corresponds to the Templates TitledPane
     }
 }
